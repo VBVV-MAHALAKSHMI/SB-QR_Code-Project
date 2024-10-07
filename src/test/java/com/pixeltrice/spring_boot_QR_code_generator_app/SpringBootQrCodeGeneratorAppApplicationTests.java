52494ec7606299a7d540082142fa01c1ac15df0e
VBVV-MAHALAKSHMI <110443757+VBVV-MAHALAKSHMI@users.noreply.github.com>
2024-10-07 22:00:52 +0530

    Add files via upload

---
+++ src/test/java/com/pixeltrice/spring_boot_QR_code_generator_app/SpringBootQrCodeGeneratorAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pixeltrice.spring_boot_QR_code_generator_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootQrCodeGeneratorAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
